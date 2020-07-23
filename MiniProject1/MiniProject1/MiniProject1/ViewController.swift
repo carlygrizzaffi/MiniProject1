09f720e85afe74563a5383652311c0f0bd0a9d87
Carly Grizzaffi <47189340+carlygrizzaffi@users.noreply.github.com>
2020-07-22 22:31:51 -0400

    Add files via upload

---
+++ MiniProject1/MiniProject1/MiniProject1/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  MiniProject1
+//
+//  Created by Carly Grizzaffi on 7/22/20.
+//  Copyright © 2020 KWK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var myName: UILabel!
+    
+    @IBOutlet weak var robotLabel: UILabel!
+    @IBOutlet weak var onlyLabel: UILabel!
+    @IBOutlet weak var movieLabel: UILabel!
+    @IBOutlet weak var bandLabel: UILabel!
+    @IBOutlet weak var debateLabel: UILabel!
+    @IBOutlet weak var violinLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func aboutMe(_ sender: Any) {
+        func pressed(sender: UIButton!) {
+            // do some stuff
+        }
+    }
+    
+}
+
